05565be83ab37acdfd80e7976a4c3de6d8bb83f7
Margarita <margo.himera@yandex.ru>
2022-07-03 22:22:26 +0300

    Update colors

--- app/src/main/java/com/example/androidhealth/ui/view/AppToolbar.kt
+++ app/src/main/java/com/example/androidhealth/ui/view/AppToolbar.kt
@@ -4,14 +4,36 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.content.withStyledAttributes
+import com.example.androidhealth.R
 import com.example.androidhealth.databinding.ViewToolbarBinding
 
 class AppToolbar @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null
+    private val attrs: AttributeSet? = null
 ) : Toolbar(context, attrs) {
 
     private val binding = ViewToolbarBinding.inflate(LayoutInflater.from(context), this)
+
+    init {
+        applyAttrs(context)
+    }
+
+    private fun applyAttrs(context: Context) {
+        context.withStyledAttributes(attrs, R.styleable.AppToolbar) {
+            val toolbarBg = getResourceId(
+                R.styleable.AppToolbar_toolbarBg,
+                R.color.componentBackground
+            )
+            val btnSelectorBg = getResourceId(
+                R.styleable.AppToolbar_btnSelectorBg,
+                R.drawable.selector_button_main_bg
+            )
+            binding.root.setBackgroundResource(toolbarBg)
+            binding.backBtn.setBackgroundResource(btnSelectorBg)
+        }
+    }
 
     fun setOnBackClickListener(listener: () -> Unit) {
         binding.backBtn.setOnClickListener { listener() }
