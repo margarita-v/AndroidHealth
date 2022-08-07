4b6a2f32d0f254b34b11dd1e48b1869fc384aba9
Margarita <margo.himera@yandex.ru>
2022-08-07 18:39:48 +0300

    Add sleep too late card

--- app/src/main/java/com/example/androidhealth/ui/sleep/SleepFragment.kt
+++ app/src/main/java/com/example/androidhealth/ui/sleep/SleepFragment.kt
@@ -3,7 +3,6 @@ package com.example.androidhealth.ui.sleep
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.example.androidhealth.R
 import com.example.androidhealth.databinding.FragmentSleepBinding
 
@@ -11,16 +10,10 @@ class SleepFragment : Fragment(R.layout.fragment_sleep) {
 
     private var binding: FragmentSleepBinding? = null
 
-    private val viewModel by viewModels<SleepViewModel>()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSleepBinding.bind(view)
         this.binding = binding
-
-        viewModel.text.observe(viewLifecycleOwner) {
-            binding.textSleep.text = it
-        }
     }
 
     override fun onDestroyView() {
