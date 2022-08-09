c71481c5f02e5d0ff382fce37fdf2b73aca77b7c
Margarita <margo.himera@yandex.ru>
2022-08-09 16:38:13 +0300

    Add pulse data generation

--- app/src/main/java/com/example/androidhealth/ui/pulse/PulseViewModel.kt
+++ app/src/main/java/com/example/androidhealth/ui/pulse/PulseViewModel.kt
@@ -1,21 +1,21 @@
 package com.example.androidhealth.ui.pulse
 
 import androidx.lifecycle.ViewModel
-import com.example.androidhealth.R
+import com.example.androidhealth.domain.PulseInfoType
 import com.example.androidhealth.ui.view.LineScaleChartView.UiData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlin.random.Random
 
 class PulseViewModel : ViewModel() {
 
-    private val _pulseData = MutableStateFlow(
-        listOf(
-            UiData(width = 224, color = R.color.errigal_white),
-            UiData(width = 16, color = R.color.jovial_jade),
-            UiData(width = 16, color = R.color.corona),
-            UiData(width = 48, color = R.color.apocalyptic_orange)
-        )
-    )
+    private val _pulseData = MutableStateFlow<List<UiData>>(listOf())
     val pulseData: StateFlow<List<UiData>> get() = _pulseData.asStateFlow()
+
+    init {
+        _pulseData.value = PulseInfoType.values().map {
+            UiData(width = Random.nextInt(0, 200), color = it.colorResId)
+        }
+    }
 }
